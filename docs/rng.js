8295517f7ed4b84bea9936a9b92eade78bf0a219
sandman9700 <150147390+sandman9700@users.noreply.github.com>
2024-03-07 10:59:05 -0500

    GT and GZ attempt 3

--- docs/rng.js
+++ docs/rng.js
@@ -97,8 +97,16 @@ $(document).ready(function () {
 
 
         const urlParams = new URLSearchParams(window.location.search);
-        urlParams.set('p1', encodeURIComponent(p1));
-        urlParams.set('p2', encodeURIComponent(p2));
+        urlParams.set('p1', 
+                      //encodeURIComponent(
+            p1
+            //         )
+            );
+        urlParams.set('p2', 
+                      //encodeURIComponent(
+                          p2
+                      //)
+                     );
         history.pushState(null, '', '?' + urlParams.toString());
 
         console.log(p1, p2)
